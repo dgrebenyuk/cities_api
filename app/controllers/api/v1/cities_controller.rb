d23e3b180dd5265e07350a7871988b9ffc3e1ddd
Denis Grebenyuk <dngrebenyuk@gmail.com>
2019-07-26 13:09:39 +0300

    add create city endpoint

--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -4,4 +4,21 @@ class Api::V1::CitiesController < ApplicationController
   def index
     render jsonapi: City.all
   end
+
+  def create
+    city = City.new(city_params)
+
+    if city.save
+      render jsonapi: city
+    else
+      render jsonapi_errors: city.errors
+    end
+  end
+
+  private
+
+  def city_params
+    params.require(:city).require([:name, :description, :population])
+    params.require(:city).permit(:name, :description, :population)
+  end
 end
