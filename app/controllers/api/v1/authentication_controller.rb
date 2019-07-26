1ecc4c7222afcd97774428eda09227f9510d5aba
Denis Grebenyuk <dngrebenyuk@gmail.com>
2019-07-26 11:35:40 +0300

    add authentication

---
+++ app/controllers/api/v1/authentication_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::AuthenticationController < ApplicationController
+  def create
+    user = User.find_by(email: user_params[:email])
+    if user&.valid_password? user_params[:password]
+      render json: { token: JsonWebToken.encode(sub: user.id) }
+    else
+      render json: { errors: ['Invalid email or password'] }
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
